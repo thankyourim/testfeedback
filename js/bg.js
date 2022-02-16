8ec8d72c18f5789fa33b9673b2757648f61b7c1b
thankyourim <95277179+thankyourim@users.noreply.github.com>
2022-02-16 17:53:05 +0900

    textfeedback

---
+++ js/bg.js
@@ -0,0 +1,8 @@
+// 'use strict';
+// const secLi= document.querySelectorAll('.section.sec1>.sec-con>ul>li');
+
+// const arrBg=[];
+// for(let i=0;i<secLi.length;i++){
+//   arrBg.push(url(img/g_0.jpg)){i});
+//   secLi[i].style
+// }
